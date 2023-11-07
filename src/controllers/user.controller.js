ad21be1acb1aad6342c4580bb301a4442bc56f63
Larry González <larrygonzalez@icsred.com>
2023-11-07 20:38:22 -0300

    [Edit name table user] -update name in database and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {pool} from '../db.js'
 
 export const getUser = async(req, res)=>{
     try {
-        const [rows] = await pool.query('select * from user')
+        const [rows] = await pool.query('select * from users')
         res.json(rows)
         
     } catch (error) {
@@ -13,7 +13,7 @@ export const getUser = async(req, res)=>{
 
 export const getOneUser = async(req, res)=>{ 
     try {
-        const[rows] = await pool.query('select * from user where id = ?',[req.params.id]);
+        const[rows] = await pool.query('select * from users where id = ?',[req.params.id]);
     
         if(rows.length <= 0) return res.status(404).json({message:'user not found'});
         res.json(rows[0]);
@@ -26,7 +26,7 @@ export const getOneUser = async(req, res)=>{
 export const createUser = async(req, res)=>{ 
     try {
         const {name,lastName, UserName,email,password,image, status,kind,create_at,update_at} = req.body;
-        const [rows] = await pool.query('Insert into user(name,lastName, UserName,email,password,image, status,kind,create_at,update_at) values (?,?,?,?,?,?,?,?,?,?)',[name,lastName, UserName,email,password,image, status,kind,create_at,update_at])
+        const [rows] = await pool.query('Insert into users(name,lastName, UserName,email,password,image, status,kind,create_at,update_at) values (?,?,?,?,?,?,?,?,?,?)',[name,lastName, UserName,email,password,image, status,kind,create_at,update_at])
         res.send({
             id: rows.insertId,
             name,lastName, UserName,email,password,image, status,kind,create_at,update_at
@@ -40,7 +40,7 @@ export const createUser = async(req, res)=>{
 
 export const deleteUser = async (req, res)=>{
     try {
-        const [result] = await pool.query('delete from user where id = ?',[req.params.id]);
+        const [result] = await pool.query('delete from users where id = ?',[req.params.id]);
     
         if(result.affectedRows <= 0) return res.status(404).json({message:'user not found'});
     
@@ -58,7 +58,7 @@ export const editUser = async (req, res)=>{
         const{id} = req.params;
         const {name,lastName, UserName,email,password,image, status,kind,create_at,update_at } = req.body;
     
-        const [result] = await pool.query('update user set name=IFNULL(?,name),lastName=IFNULL(?,lastName), UserName=IFNULL(?,UserName),email=IFNULL(?,email),password=IFNULL(?,password),image=IFNULL(?,image), status=IFNULL(?,status),kind=IFNULL(?,kind),create_at=IFNULL(?,create_at),update_at=IFNULL(?,update_at) where id = ?',[name,lastName, UserName,email,password,image, status,kind,create_at,update_at,id]);
+        const [result] = await pool.query('update users set name=IFNULL(?,name),lastName=IFNULL(?,lastName), UserName=IFNULL(?,UserName),email=IFNULL(?,email),password=IFNULL(?,password),image=IFNULL(?,image), status=IFNULL(?,status),kind=IFNULL(?,kind),create_at=IFNULL(?,create_at),update_at=IFNULL(?,update_at) where id = ?',[name,lastName, UserName,email,password,image, status,kind,create_at,update_at,id]);
     
         if (result.affectedRows === 0) return res.status(404).json({message:'user not found'});
         res.json('recived')
